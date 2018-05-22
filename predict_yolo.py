28954afc17b4a5f72454a0390bd00d8ea16c632e
Satoshi Shimada <mylinux1204@gmail.com>
2018-05-23 00:26:38 +0900

    update predictor of yolo

--- predict_yolo.py
+++ predict_yolo.py
@@ -1,7 +1,7 @@
 import time
 import cv2
 import numpy as np
-from chainer import serializers, Variable
+from chainer import serializers, Variable, cuda
 import chainer.functions as F
 import argparse
 from model import YOLOv2, YOLOv2Predictor
@@ -10,9 +10,9 @@ from lib.utils import Box
 
 fcn = False
 class Predictor:
-    def __init__(self):
+    def __init__(self, gpu=0):
         # hyper parameters
-        weight_file = "./weight/model_snapshot_fcn_300"
+        weight_file = "./weight/fcn-un4-100"
         self.n_classes_fcn = 7
         self.n_classes_yolo = 2
         self.n_boxes = 5
@@ -26,7 +26,11 @@ class Predictor:
         yolov2 = YOLOv2(n_classes_fcn=self.n_classes_fcn, n_classes_yolo=self.n_classes_yolo, n_boxes=self.n_boxes)
         model = YOLOv2Predictor(yolov2, FCN=fcn)
         serializers.load_npz(weight_file, model)
+        if gpu >= 0:
+            cuda.get_device(gpu).use()
+            model.to_gpu()
         self.model = model
+        self.gpu = gpu
 
     def __call__(self, img):
         orig_input_height, orig_input_width, _ = img.shape
@@ -39,7 +43,10 @@ class Predictor:
         # forward
         x_data = img[np.newaxis, :, :, :]
         x = Variable(x_data)
-        x, y, w, h, conf, prob = self.model.predict(x)
+        if self.gpu >= 0:
+            x.to_gpu()
+        pred = self.model.predict(x)
+        x, y, w, h, conf, prob = pred
 
         # parse results
         _, _, _, grid_h, grid_w = x.shape
@@ -50,6 +57,14 @@ class Predictor:
         conf = F.reshape(conf, (self.n_boxes, grid_h, grid_w)).data
         prob = F.transpose(F.reshape(prob, (self.n_boxes, self.n_classes_yolo, grid_h, grid_w)), (1, 0, 2, 3)).data
         detected_indices = (conf * prob).max(axis=0) > self.detection_thresh
+        if self.gpu >= 0:
+            x = cuda.to_cpu(x)
+            y = cuda.to_cpu(y)
+            w = cuda.to_cpu(w)
+            h = cuda.to_cpu(h)
+            conf = cuda.to_cpu(conf)
+            prob = cuda.to_cpu(prob)
+            detected_indices = cuda.to_cpu(detected_indices)
 
         results = []
         for i in range(detected_indices.sum()):
@@ -72,14 +87,15 @@ class Predictor:
 if __name__ == "__main__":
     # argument parse
     parser = argparse.ArgumentParser(description="predict image")
-    parser.add_argument('path', help="input image path")
+    parser.add_argument('--path', help="input image path")
+    parser.add_argument('--gpu', default=0, type=int)
     args = parser.parse_args()
     image_file = args.path
 
     # read image
     orig_img = cv2.imread(image_file)
 
-    predictor = Predictor()
+    predictor = Predictor(gpu=args.gpu)
     nms_results = predictor(orig_img)
 
     # draw result
